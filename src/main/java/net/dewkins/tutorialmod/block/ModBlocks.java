35fe90545f9c142a0b43b677ad5911eca56edec2
Dewkins <dewkins8@hotmail.com>
2023-02-02 17:39:19 +0100

    Updated with Blocks

--- src/main/java/net/dewkins/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/dewkins/tutorialmod/block/ModBlocks.java
@@ -34,6 +34,14 @@ public class ModBlocks {
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)), ModCreativeModeTab.TUTORIAL_TAB);
+    public static final RegistryObject<Block> NETHERRACK_ZIRCON_ORE = registerBlock("netherrack_zircon_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops(),
+                    UniformInt.of(3,7)), ModCreativeModeTab.TUTORIAL_TAB);
+    public static final RegistryObject<Block> END_STONE_ZIRCON_ORE = registerBlock("end_stone_zircon_ore",
+            () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops(),
+                    UniformInt.of(3,7)), ModCreativeModeTab.TUTORIAL_TAB);
 
 
 
